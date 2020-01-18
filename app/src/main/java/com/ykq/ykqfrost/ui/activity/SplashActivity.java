ee28052c82b257c4d00224a601cb4f884b9e9d5d
ykqfrost <yaokeqi1996@outlook.com>
2020-01-18 09:17:28 +0800

    导航栏遮住启动页底部问题

--- app/src/main/java/com/ykq/ykqfrost/ui/activity/SplashActivity.java
+++ app/src/main/java/com/ykq/ykqfrost/ui/activity/SplashActivity.java
@@ -1,8 +1,6 @@
 package com.ykq.ykqfrost.ui.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
@@ -16,15 +14,7 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
-
-    private void hideNavigationBar() {
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
     }
 }
