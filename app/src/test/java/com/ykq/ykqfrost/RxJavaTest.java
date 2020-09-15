1bdc73aca163bfb9593354b5930a679708681808
ykqfrost <yaokeqi1996@outlook.com>
2020-09-15 14:07:08 +0800

    rxjava测试使用

---
+++ app/src/test/java/com/ykq/ykqfrost/RxJavaTest.java
@@ -0,0 +1,88 @@
+package com.ykq.ykqfrost;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+/**
+ * @author ykq
+ * @date 2020/4/30
+ */
+public class RxJavaTest {
+
+    @Test
+    public void threadTest() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                System.out.println("Observable thread is : " + Thread.currentThread().getName());
+                e.onNext(1);
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        System.out.println("After observeOn(mainThread)，Current thread is " + Thread.currentThread().getName());
+                    }
+                })
+                .observeOn(Schedulers.io())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        System.out.println("After observeOn(io)，Current thread is " + Thread.currentThread().getName());
+                    }
+                });
+    }
+
+    @Test
+    public void test() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Throwable {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onComplete();
+                e.onNext(4);
+            }
+        }).subscribe(new Observer<Integer>() {
+            private int i;
+            private Disposable mDisposable;
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                i++;
+                System.out.println(integer);
+                if (i == 2) {
+                    mDisposable.dispose();
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+}
